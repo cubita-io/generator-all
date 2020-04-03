9bc533589d15c438c1e1c14d88ad320cb2f7245a
cubita-io <cubitadotio@sina.com>
2020-04-03 10:27:37 +0800

    代码优化

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # generator-all
-代码自动生成
+* 代码自动生成
+* cp target/generator-all-3.3.1.jar ../template/tools 
--- src/main/java/io/cubita/generator/CodeGenerator.java
+++ src/main/java/io/cubita/generator/CodeGenerator.java
@@ -16,9 +16,12 @@ import com.baomidou.mybatisplus.generator.config.FileOutConfig;
 import com.baomidou.mybatisplus.generator.config.GlobalConfig;
 import com.baomidou.mybatisplus.generator.config.PackageConfig;
 import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.po.LikeTable;
 import com.baomidou.mybatisplus.generator.config.po.TableInfo;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import static com.baomidou.mybatisplus.core.enums.SqlLike.RIGHT;
 
 /**
  * <p>
@@ -33,6 +36,7 @@ public class CodeGenerator {
 
     public static void main(String[] args) {
         String projectPath = System.getProperty("projectPath");
+//        String moudleName = System.getProperty("moudleName");
         String database = System.getProperty("database");
         String pkgName = System.getProperty("pkgName");
         String tablePrefix = System.getProperty("tablePrefix");
@@ -40,14 +44,22 @@ public class CodeGenerator {
         String port = System.getProperty("port");
         String userName = System.getProperty("userName");
         String userPwd = System.getProperty("userPwd");
+        String likeTable = System.getProperty("likeTable");
+        boolean resXml = Boolean.parseBoolean(System.getProperty("resXml", "false"));
 
-        String help = "-DprojectPath=? -Ddatabase=? -DpkgName=? -DtablePrefix=* -Dhost=127.0.0.1 -Dport=3306 -DuserName=root -DuserPwd=123456";
+        String help = "-DprojectPath=? -DmoudleName=? -Ddatabase=? -DpkgName=? -DlikeTable=? -DtablePrefix=* -Dhost=127.0.0.1 -Dport=3306 -DuserName=root -DuserPwd=123456 -DresXml=false";
 
         if (!StringUtils.hasText(projectPath)) {
             logger.warn("请配置projectPath");
             logger.info("用法:" + help);
             return;
         }
+
+//        if (!StringUtils.hasText(moudleName)) {
+//            logger.warn("请配置moudleName");
+//            logger.info("用法:" + help);
+//            return;
+//        }
 
         if (!StringUtils.hasText(database)) {
             logger.warn("请配置database");
@@ -111,26 +123,31 @@ public class CodeGenerator {
             @Override
             public void initMap() {
                 this.getConfig().getPathInfo().remove(ConstVal.CONTROLLER_PATH);
-//                this.getConfig().getPathInfo().remove(ConstVal.XML_PATH);
+                if (resXml) {
+                    //移除默认生成的方式
+                    this.getConfig().getPathInfo().remove(ConstVal.XML_PATH);
+                }
             }
         };
         mpg.setCfg(cfg);
 
         // 如果模板引擎是 freemarker
-        String templatePath = "/templates/mapper.xml.ftl";
-        // 自定义输出配置
-        List<FileOutConfig> focList = new ArrayList<>();
-        // 自定义配置会被优先输出
-        focList.add(new FileOutConfig(templatePath) {
-            @Override
-            public String outputFile(TableInfo tableInfo) {
-                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
-                return projectPath + "/src/main/resources/mapper/" + pc.getModuleName()
-                        + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
-            }
-        });
-        cfg.setFileOutConfigList(focList);
-        mpg.setCfg(cfg);
+        if (resXml) {
+            String templatePath = "/templates/mapper.xml.ftl";
+            // 自定义输出配置
+            List<FileOutConfig> focList = new ArrayList<>();
+            // 自定义配置会被优先输出
+            focList.add(new FileOutConfig(templatePath) {
+                @Override
+                public String outputFile(TableInfo tableInfo) {
+                    // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                    return projectPath + "/src/main/resources/mapper/dao" +
+                            "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+                }
+            });
+            cfg.setFileOutConfigList(focList);
+            mpg.setCfg(cfg);
+        }
 
         StrategyConfig strategy = new StrategyConfig();
 
@@ -141,6 +158,9 @@ public class CodeGenerator {
 
         if (StringUtils.hasText(tablePrefix)) {
             strategy.setTablePrefix(tablePrefix);
+        }
+        if (StringUtils.hasText(likeTable)) {
+            strategy.setLikeTable(new LikeTable(likeTable, RIGHT));
         }
         mpg.setStrategy(strategy);
 
